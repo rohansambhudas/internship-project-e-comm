6be2d253c9f3774132ae9bcdef81918de5494145
Rohan Sambhudas <rohan.sambhudas@gmail.com>
2023-04-26 04:46:12 +0000

    jwt and forgot password token added

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,9 @@ dotenv.config()
 
 const config = {
     PORT: process.env.PORT || 5000,
-    MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/ecomm"
+    MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/ecomm",
+    JWT_TOKEN: process.env.JWT_TOKEN || "yoursecret",
+    JWT_EXPIRY: process.env.JWT_EXPIRY || "7d"
 }
 
 export default config;
